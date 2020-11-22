98359155a4963bfdded4cd83c804fc9370e35c72
Archana-Bhamare <archusonar2@gmail.com>
2020-11-22 21:37:13 +0530

    Added : Rolling the Die

--- src/com/Bridgelabz/SnakeandLadder/SnakeAndLadderGame.java
+++ src/com/Bridgelabz/SnakeandLadder/SnakeAndLadderGame.java
@@ -5,5 +5,7 @@ public class SnakeAndLadderGame {
 	int position = 0;
 
 	public static void main(String[] args) {
+		int roll_Dice = (int) ((Math.random() * 10 % 6) +1);
+		System.out.println("Die Value : " + roll_Dice);
 	}
 }
