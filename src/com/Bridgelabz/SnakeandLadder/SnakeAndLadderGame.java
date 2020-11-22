22cb3aaf4073fff55881c03168c0361df626106c
Archana-Bhamare <archusonar2@gmail.com>
2020-11-22 21:17:13 +0530

    Added : Welcome to the Snake and Ladder Game

---
+++ src/com/Bridgelabz/SnakeandLadder/SnakeAndLadderGame.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz.SnakeandLadder;
+
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Snake and Ladder Game...");
+	}
+}
